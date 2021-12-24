415ad9f85169e9198b0e444480660f5e8fe71945
Andre Burke <aburke.dev@gmail.com>
2021-12-23 22:38:32 -0700

    added auth config

---
+++ src/authConfig.ts
@@ -0,0 +1,22 @@
+import { Configuration, PopupRequest, RedirectRequest } from "@azure/msal-browser";
+import { reactMsalClientId, reactMsalAuthority, reactMsalRedirectUri } from "./env.local";
+
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: reactMsalClientId,
+    authority: reactMsalAuthority,
+    redirectUri: reactMsalRedirectUri,
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  }
+};
+
+export const loginPopupRequest: PopupRequest = {
+  scopes: ["openid", "profile", "User.Read"]
+}
+
+export const loginRedirectRequest: RedirectRequest = {
+  scopes: ["openid", "profile", "User.Read"]
+}
