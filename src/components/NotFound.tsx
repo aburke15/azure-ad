c1ce2c311dd8f8609c806d47509400d9e960f324
Andre Burke <aburke.dev@gmail.com>
2021-12-28 22:22:56 -0700

    added a not found component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="container mt-5">
+      <h3 className="mt-2">404</h3>
+      <Link className="btn btn-secondary" to="/">
+        <i>Click Here</i> to go back
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
